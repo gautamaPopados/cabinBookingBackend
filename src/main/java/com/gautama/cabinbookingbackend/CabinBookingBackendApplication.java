e432325990caa86ed4c9690d43fd7cc01a38e061
Egor Morozov <egornexus10@gmail.com>
2025-06-28 15:17:50 +0700

    init commit

---
+++ src/main/java/com/gautama/cabinbookingbackend/CabinBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gautama.cabinbookingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinBookingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabinBookingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gautama/cabinbookingbackend/CabinBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gautama.cabinbookingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinBookingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
